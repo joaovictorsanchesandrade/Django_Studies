4c1fa15b861736da6a325679ef3dc687d7420ccf
João Victor Sanches Andrade <joaovictorsanchesandradeestuda@gmail.com>
2026-01-13 11:05:50 -0300

    Create details product

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest
+from .models import Product
 
 # Create your views here.
+def product_view(request: HttpRequest, slug: str):
+    product = get_object_or_404(Product, slug=slug, available=True)
+    return render(request, 'products/detail.html', {'product': product})
