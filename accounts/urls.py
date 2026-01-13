009470ee797d299735d2e65e4f059f2b748eb19a
João Victor Sanches Andrade <joaovictorsanchesandradeestuda@gmail.com>
2026-01-13 10:26:31 -0300

    Create Login, Register and Logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path('login', views.login_view, name='login')
+    path('login', views.UserLoginView.as_view(), name='login'),
+    path('register', views.UserCreationView.as_view(), name='register'),
+    path('logout', views.UserLogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 
-# Create your views here.
-def login_view(request):
-    pass
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    
+
+class UserLogoutView(LogoutView):
+    next_page = 'accounts:login'
+
+class UserCreationView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('core:index')
+
+    def form_valid(self, form):
+        """ Automatic login """
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.success_url)
